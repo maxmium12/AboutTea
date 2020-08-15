f1df5975b42b9629732ec5ea9ba44043783dd24b
maxmium12 <1141865879@qq.com>
2020-08-15 11:32:58 +0800

    更新

--- src/main/java/com/maximum/abouttea/block/machine/BlockTeaStoneWorkStation.java
+++ src/main/java/com/maximum/abouttea/block/machine/BlockTeaStoneWorkStation.java
@@ -9,13 +9,17 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
+
+import javax.annotation.Nullable;
 
 public class BlockTeaStoneWorkStation extends Block {
 
@@ -36,5 +40,16 @@ public class BlockTeaStoneWorkStation extends Block {
             }
         }
         return ActionResultType.PASS;
+    }
+
+    @Override
+    public boolean hasTileEntity() {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+        return ModTiles.TEA_STONE_WORKSTATION.get().create();
     }
 }
