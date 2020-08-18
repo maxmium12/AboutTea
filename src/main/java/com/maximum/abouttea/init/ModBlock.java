9b9c8259ee26674f44647d953dde47974d49f3d9
MaxSeth <41491109+maxmium12@users.noreply.github.com>
2020-08-18 13:04:52 +0000

    Update ModBlock.java

--- src/main/java/com/maximum/abouttea/init/ModBlock.java
+++ src/main/java/com/maximum/abouttea/init/ModBlock.java
@@ -31,9 +31,9 @@ import java.util.function.Supplier;
 import static com.maximum.abouttea.AboutTea.MODID;
 
 public class ModBlock {
-    public static RegistryObject<BlockTeaSet> blockTeaSet;//= RegistryUtil.registryBlockWithItem("tea_set",BlockTeaSet::new);
-    public static RegistryObject<BlockTeaTreeLeave> blockTeaTreeLeave;//=RegistryUtil.registryBlockWithItem("tea_leaves",()->new BlockTeaTreeLeave(Block.Properties.create(Material.LEAVES).hardnessAndResistance(0.2f),"greentea"));
-    public static RegistryObject<BlockTeaWood> blockTeaWood;//=RegistryUtil.registryBlockWithItem("tea_wood",BlockTeaWood::new);
+    public static RegistryObject<BlockTeaSet> blockTeaSet= RegistryUtil.registryBlockWithItem("tea_set",BlockTeaSet::new);
+    public static RegistryObject<BlockTeaTreeLeave> blockTeaTreeLeave=RegistryUtil.registryBlockWithItem("tea_leaves",()->new BlockTeaTreeLeave(Block.Properties.create(Material.LEAVES).hardnessAndResistance(0.2f),"greentea"));
+    public static RegistryObject<BlockTeaWood> blockTeaWood=RegistryUtil.registryBlockWithItem("tea_wood",BlockTeaWood::new);
     public static RegistryObject<BlockTeaStoneOre> blockTeaStoneOre=RegistryUtil.registryBlockWithItem("tea_stone_ore",BlockTeaStoneOre::new);
     public static RegistryObject<BlockTeaGenerator> blockTeaGenerator=RegistryUtil.registryBlockWithItem("tea_generator",BlockTeaGenerator::new);
     public static RegistryObject<BlockTeaBookConverter> blockTeaBookConverter=RegistryUtil.registryBlockWithItem("tea_book_converter",BlockTeaBookConverter::new);
@@ -43,7 +43,7 @@ public class ModBlock {
     public static RegistryObject<BlockTeaDryer> blockManualTeaDryer=RegistryUtil.registryBlockWithItem("manual_tea_dryer",BlockTeaDryer::new);
     public static RegistryObject<BlockTeaMixer> blockManualTeaMixer=RegistryUtil.registryBlockWithItem("manual_tea_mixer",BlockTeaMixer::new);
     public static RegistryObject<BlockTeaStoneWorkStation> blockTeaStoneWorkStation = RegistryUtil.registryBlockWithItem("tea_stone_workstation", BlockTeaStoneWorkStation::new);
-    public static RegistryObject<BlockTeaStone> blockTeaStone;// = RegistryUtil.registryBlockWithItem("tea_stone_block",BlockTeaStone::new);
+    public static RegistryObject<BlockTeaStone> blockTeaStone= RegistryUtil.registryBlockWithItem("tea_stone_block",BlockTeaStone::new);
     public static RegistryObject<BlockWire> blockWire = RegistryUtil.registryBlockWithItem("wire", BlockWire::new);
     public static RegistryObject<BlockMachineDryer> blockMachineDryer = RegistryUtil.registryBlockWithItem("machine_dryer", BlockMachineDryer::new);
     public static RegistryObject<BlockMachineMixer> blockMachineMixer = RegistryUtil.registryBlockWithItem("machine_mixer", BlockMachineMixer::new);
