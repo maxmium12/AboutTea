7da2b12d3d3e8576b7d69c08ce862571b3dfa1f5
MaxSeth <41491109+maxmium12@users.noreply.github.com>
2020-08-18 12:34:51 +0000

    Update AboutTea.java

--- src/main/java/com/maximum/abouttea/AboutTea.java
+++ src/main/java/com/maximum/abouttea/AboutTea.java
@@ -16,7 +16,7 @@ public class AboutTea {
     public static final String MODID="abouttea";
     public AboutTea(){
         instance=this;
-        //new ModBlock();
+        new ModBlock();
         new ModTiles();
         new ModFeature();
         new ModItems();
